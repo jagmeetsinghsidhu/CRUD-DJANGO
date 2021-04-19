7193c7e0255a9ea9cea95666d1bab1ab194a6647
JAGMEET SINGH SIDHU <jagmeetsinghsidhu52@gmail.com>
2021-04-18 21:57:59 -0400

    First commit

---
+++ rapidapipractice/rapidapipractice/api/settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS = [
+  ‘django.contrib.admin’,
+  ‘django.contrib.auth’,
+  ‘django.contrib.contenttypes’, 
+  ‘django.contrib.sessions’,
+  ‘django.contrib.messages’,
+  ‘django.contrib.staticfiles’,
+‘api’,
+  ‘rest_framework’,
+]
+# Heroku: Update database configuration from $DATABASE_URL. 
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env
